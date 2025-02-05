d0ad21b026a27231176001742792548b8ba22955
wwIrvingww <mor22781@uvg.edu.gt>
2025-02-05 10:43:27 -0600

    feat: reader json example

--- direct_afd_construction/src/main.rs
+++ direct_afd_construction/src/main.rs
@@ -1,3 +1,26 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+
+#[derive(Deserialize, Debug)]
+struct AFD {
+    Q: Vec<String>,             // Lista de estados
+    Σ: Vec<String>,             // Alfabeto
+    q0: String,                 // Estado inicial
+    F: Vec<String>,             // Conjunto de estados finales
+    δ: HashMap<String, String>, // Función de transición (mapeo de transiciones)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./automatas/afd_example.json").unwrap();
+    let reader = BufReader::new(file);
+    let afd: AFD = serde_json::from_reader(reader).unwrap();
+
+    // Imprime los campos de la estructura AFD
+    println!("Q: {:?}", afd.Q);
+    println!("Σ: {:?}", afd.Σ);
+    println!("q0: {}", afd.q0);
+    println!("F: {:?}", afd.F);
+    println!("δ: {:?}", afd.δ);
 }
